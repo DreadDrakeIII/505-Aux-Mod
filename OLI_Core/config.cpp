6aeac97935667418ef5d680b7d6dad285f200569
Winter <coldwinterproductions41st@gmail.com>
2025-02-02 23:21:48 -0500

    Added factions + Jet
    Addition of factions (core) and F/A-Warhawk (vehicles)

--- OLI_Core/config.cpp
+++ OLI_Core/config.cpp
@@ -18,133 +18,99 @@ class CfgPatches
 
 class CfgFactionClasses
 {
-	class TCI_Fac
+	class OLI_Fac
 	{
-		displayName="Clone Insurrectionists";
+		displayName="505th Expeditionary Force";
 		side=1;
 	};
-	class TCI_Fac_opfor
-	{
-		displayName="(TCI) Imperial Units";
-		side=0;
-	};
+
 };
 class CfgEditorCategories
 {
-	class TCI_Objects
+	class OLI_Objects
 	{
 		scope=2;
 		scopeCurator=2;
-		displayName="TCI Assets";
+		displayName="505th Assets";
 	};
-		class TCI_Imp_trooper
-	{
-		displayname="(TCI) Galactic Empire";
-		side=0;
-		scopecurator=2;
-		scope=2;
-	};
+
 };
 class CfgEditorSubcategories
 {
-	class TCI_INF
+	class OLI_INF
 	{
 		scope=2;
 		scopeCurator=2;
 		displayName="Troopers";
 	};
-	class TCI_Landcraft
+	class OLI_Landcraft
 	{
 		scope=2;
 		scopeCurator=2;
 		displayName="Landcraft";
 	};
-	class TCI_Aircraft_RTW
+	class OLI_Aircraft_RTW
 	{
 		scope=2;
 		scopeCurator=2;
 		displayName="Aircraft (Rotary Wing)";
 	};
-	class TCI_Aircraft_FXW
+	class OLI_Aircraft_FXW
 	{
 		scope=2;
 		scopeCurator=2;
 		displayName="Aircraft (Fixed Wing)";
 	};
-	class TCI_Helmets_Props_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Helmet Props (Rank)";
-	};
-	class TCI_Helmets_Props_cus_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Helmet Props (Custom)";
-	};
-	class TCI_Flags_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Flags";
-	};
-	class TCI_Ships_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Ships";
-	};
-	class TCI_Ship_Parts_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Ship Parts";
-	};
-	class TCI_resupply_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Resupply Crates";
-	};
-	class TCI_gonk_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Gonks";
-	};
-		class TCI_Imp_trooper_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="(TCI) Stormtroopers";
-	};
-	class TCI_Imp_scout_trooper_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Scout Troopers";
-	};
-	class TCI_Imp_snow_trooper_subcat
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Snowtroopers";
-	};
-	class TCI_IMP_GND_VEHIC
-	{
-		scope=2;
-		scopeCurator=2;
-		displayname="Ground Vehicles";
-	};
+	//class TCI_Helmets_Props_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Helmet Props (Rank)";
+	//};
+	//class TCI_Helmets_Props_cus_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Helmet Props (Custom)";
+	//};
+	//class TCI_Flags_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Flags";
+	//};
+	//class TCI_Ships_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Ships";
+	//};
+	//class TCI_Ship_Parts_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Ship Parts";
+	//};
+	//class TCI_resupply_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Resupply Crates";
+	//};
+	//class TCI_gonk_subcat
+	//{
+	//	scope=2;
+	//	scopeCurator=2;
+	//	displayname="Gonks";
+	//};
 };
-class CfgMarkerClasses
-{
-	class TCI_MapMarker_Classes
-	{
-		scope=2;
-		scopeCurator=2;
-		Priority=1
-		displayName="TCI Markers";
-	};
-};
+//class CfgMarkerClasses
+//{
+//	class TCI_MapMarker_Classes
+//	{
+//		scope=2;
+//		scopeCurator=2;
+//		Priority=1
+	//	displayName="TCI Markers";
+//	};
+//};
---
+++ OLI_Vehicles/Data/Warhawk/config.cpp
@@ -0,0 +1,57 @@
+class CfgPatches
+{
+	class OLI_Aircraft_Warhawk
+	{
+		author="Cold Winter Productions";
+		addonRootClass="OLI_Vehicle_Core";
+		requiredAddons[]=
+		{
+			"OLI_Vehicle_Core",
+			"OLI_Core"
+		};
+		requiredVersion=0.1;
+		units[]=
+		{
+		"OLI_Warhawk"
+		};
+		weapons[]={};
+		skipWhenMissingDependencies = 1;
+	};
+};
+class CfgVehicles
+{
+	class B_Plane_Fighter_01_F;
+	
+/////////////////////////////////////////////////////////
+	
+	class OLI_Warhawk: B_Plane_Fighter_01_F
+	{
+        displayName = "F/A-40 Warhawk";
+        faction = "OLI_Fac";
+        editorsubcategory = "OLI_Aircraft_FXW";
+        //crew = "TCI_unit";
+		weapons[]=
+		{
+
+		};
+		
+		magazines[]=
+		{
+
+		};
+		//armor=150;
+       hiddenSelectionsTextures[]=
+		{
+			"OLI_Vehicles\Data\Warhawk\data\fighter_01_fuselage_01_co.paa",
+			"OLI_Vehicles\Data\Warhawk\data\fighter_01_fuselage_02_co.paa",
+			"a3\air_f_jets\plane_fighter_01\data\fighter_01_glass_01_ca.paa",
+			"a3\air_f_jets\plane_fighter_01\data\fighter_01_cockpit_01_co.paa",
+			"a3\air_f_jets\plane_fighter_01\data\fighter_01_cockpit_02_co.paa",
+			"a3\air_f_jets\plane_fighter_01\data\fighter_01_cockpit_03_co.paa",
+			"a3\air_f_jets\plane_fighter_01\data\fighter_01_cockpit_05_co.paa",
+			"a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa",
+			"a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_00_ca.paa",
+			"a3\air_f_jets\plane_fighter_01\data\Numbers\Fighter_01_01_ca.paa"
+		};
+   };
+};
