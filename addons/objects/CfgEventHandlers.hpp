760535467d9c71e9b895b1f01cb8038751a376bf
DreadDrakeIII <103012855+DreadDrakeIII@users.noreply.github.com>
2025-10-15 13:42:52 -0700

    Update CfgEventHandlers.hpp

--- addons/objects/CfgEventHandlers.hpp
+++ addons/objects/CfgEventHandlers.hpp
@@ -9,3 +9,11 @@ class Extended_PreInit_EventHandlers {
         init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
     };
 };
+
+class Extended_InitPost_EventHandlers {
+    class CLASS(Marine_Loadout) {
+        class GVAR(LockerInit) {
+            clientInit = QUOTE(_this call FUNC(LockerInit));
+        };
+    };
+};
